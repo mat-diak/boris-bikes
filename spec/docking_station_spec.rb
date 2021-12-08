9fdea5b91fbc4b3ffe67eee28d4c6900502fc6ff
SophieGowen <sophie.j.gowen@gmail.com>
2021-12-08 14:54:35 +0000

    Add test for release_bike and release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-    
+    # it "responds to release_bike" do
+    #     expect(DockingStation).to respond_to(release_bike)
+    # end
+    it { is_expected.to respond_to(:release_bike) }
 end
