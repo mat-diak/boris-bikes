eb57854ce50c6c8ffcdef6cdd4707072e4425b49
mikitaro-27 <matt.diak@gmail.com>
2021-12-08 15:56:29 +0000

    add test for working? on docking_station.release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,14 @@
 require 'docking_station'
 
-describe DockingStation do
-    # it "responds to release_bike" do
-    #     expect(DockingStation).to respond_to(release_bike)
-    # end
-    it { is_expected.to respond_to(:release_bike) }
-    
-    bike = DockingStation.release_bike
-    expect(bike).to respond_to(:working?)
-end
-
-# describe DockingStation.release_bike do
-#     it { is_expected.to respond_to(:working?) }
+# This is working !
+# describe 'bike' do
+#   let (:bike) {DockingStation.release_bike}
+#   it { is_expected.to respond_to(:working?) }
 # end
 
-
+describe DockingStation do
+    it { is_expected.to respond_to(:release_bike) }
+    describe :release_bike do
+      it { is_expected.to respond_to(:working?) }
+    end
+end
