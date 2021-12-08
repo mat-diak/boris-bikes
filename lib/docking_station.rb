fa836fed1406ed0252fc28b4110cd16453a6633e
mikitaro-27 <matt.diak@gmail.com>
2021-12-08 16:45:26 +0000

    feed DockingStation class 'bike' parameter; delete redundant bike.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,4 @@
-require "bike"
+require './lib/bike'
 
 class DockingStation
     def initialize(bike)
@@ -6,6 +6,22 @@ class DockingStation
     end
     
     def release_bike
-        @bike.new
+        @bike.working?
+    end
+
+    def print
+        p @bike.is_a? (Bike)
+        p @bike
     end
 end
+
+# TEST!!
+# bike = Bike.new
+# docking_station = DockingStation.new(bike)
+# docking_station.release_bike
+# docking_station.print
+
+
+# use irb
+# require bike and docking station
+# try to get docking_station to use bike
