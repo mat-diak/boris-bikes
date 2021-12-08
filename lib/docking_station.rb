1f20f1c8aab0e116b99c1c4274bbce42eb084e25
mikitaro-27 <matt.diak@gmail.com>
2021-12-08 18:01:32 +0000

    fix test for working? to be called on DockingStation.release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,27 +1,7 @@
 require './lib/bike'
 
 class DockingStation
-    # def initialize
-    #     @bike = Bike.new
-    # end
-    
     def release_bike
         Bike.new
     end
-
-    # def print
-    #     p @bike.is_a? (Bike)
-    #     p @bike
-    # end
 end
-
-# TEST!!
-# bike = Bike.new
-# docking_station = DockingStation.new(bike)
-# docking_station.release_bike
-# docking_station.print
-
-
-# use irb
-# require bike and docking station
-# try to get docking_station to use bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,15 @@
 require 'docking_station'
-require 'bike'
 
 # This is working !
-# describe 'bike' do
+# describe bike do
 #     let (:bike) {DockingStation.release_bike}
 #     it { is_expected.to respond_to(:working?) }
 # end
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
-    describe '.release_bike' do
-      it { is_expected.to respond_to(:working?) }
-    end
+end
+
+describe DockingStation.new.release_bike do
+  it { is_expected.to respond_to(:working?) }
 end
