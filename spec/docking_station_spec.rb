a53a1cfee4d151237717bd2f197f485d1292b366
Haydn Stokes <haydnstokes92@gmail.com>
2021-12-09 15:16:57 +0000

    Fixed Method, updating unit tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,11 +10,19 @@ require 'docking_station'
 # Try if DockingStation responds to dock_bike
 
 describe DockingStation do
-    it { is_expected.to respond_to(:release_bike) }
-    it { is_expected.to respond_to(:dock_bike) }
+  it { is_expected.to respond_to(:release_bike) }
+    # update unit test so they match output needed for feature test
+end
+
+describe DockingStation do
+  it "docks somthing" do
+  Bike.new
+  it { is_expected.to respond_to(:dock_bike) }
 end
 
 describe DockingStation.new.release_bike do
   it { is_expected.to respond_to(:working?) }
 end
 
+# update the test for dock
+# we to return the bike we dock
