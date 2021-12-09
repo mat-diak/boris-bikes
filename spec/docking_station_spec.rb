3e069f9b5b158f28f75cd422e764fc8d79f104d8
mikitaro-27 <matt.diak@gmail.com>
2021-12-09 14:35:13 +0000

    add rspec test for dock_bike on DockingStation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,10 +6,15 @@ require 'docking_station'
 #     it { is_expected.to respond_to(:working?) }
 # end
 
+# Write failing test
+# Try if DockingStation responds to dock_bike
+
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
+    it { is_expected.to respond_to(:dock_bike) }
 end
 
 describe DockingStation.new.release_bike do
   it { is_expected.to respond_to(:working?) }
 end
+
