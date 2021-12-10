5ffa89b86297010e01541d4a7149a0516b904e0c
mikitaro-27 <matt.diak@gmail.com>
2021-12-10 14:26:02 +0000

    test for raising error when no bikes available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -14,6 +14,11 @@ class DockingStation
 
     def dock_bike(bike)
         @dock << bike
+        raise "There is no bikes available"
     end
 
 end
+
+# Use the fail or raise keyword to raise an exception in your code
+
+# Lets raise an error
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,8 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock_bike) }
 
-  let(:docking_station) { DockingStation.new }
-  let(:bike) { docking_station.release_bike }
+  let(:docking_station) { DockingStation.new } # docking_station = DockingStation.new
+  let(:bike) { docking_station.release_bike } # bike = docking_station.release_bike
   let(:return_bike) { docking_station.dock_bike(bike) }
   
   it "should create a dock attribute with an empty array at startup" do
@@ -20,19 +20,22 @@ describe DockingStation do
   it 'releases working bikes' do
     expect(bike).to be_working
   end
+
+  it 'there is no bikes; throws an error message when we release a bike ' do
+    expect { docking_station.release_bike }.to raise_error("There are no bikes available")
+  end
 end
 
 # There are no bikes in the docking station. We want our code to produce an error,
 # when using the release_bike method.
 
-We want an error to occur when we release a bike. 
- it 'throws an error message when we release a bike'
-  expect { docking_station.release_bike }.to raise_error("There are no bikes available")
-end 
-
+# We want an error to occur when we release a bike. 
+ 
 # Arrange
+# We need to have a docking_station
 
 # Act
 
 # Assert
 
+# 
