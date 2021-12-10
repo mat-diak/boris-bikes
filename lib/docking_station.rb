7c47752fd365e055aadc15a58ed655177d744e81
mikitaro-27 <matt.diak@gmail.com>
2021-12-10 11:06:53 +0000

    correct bike in rspec to come from DockingStation class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,9 @@
+require 'pry'
 require './lib/bike'
 
 class DockingStation
     attr_reader :dock
+    
     def initialize 
         @dock = []
     end
@@ -11,11 +13,6 @@ class DockingStation
     end
 
     def dock_bike(bike)
-        "docked"
-    # set n attribute on an instantiated object eg bike using the @ instance variable
+        @dock << bike
     end
 end
-
-# GOAL - we wanna have a place to store the bike - using instance varaibale
-# create an array in the class that stores the bikes
-# when dock_bike is called we put that bike as parameter
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,39 +3,21 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock_bike) }
-end
 
-# creating to store bikes in the dock
-describe DockingStation do
-  it "Check if dock attribute is created" do
-  docking_station = DockingStation.new
+  let(:docking_station) { DockingStation.new }
+  let(:bike) { docking_station.release_bike }
+  let(:return_bike) { docking_station.dock_bike(bike) }
+  
+  it "should create a dock attribute with an empty array at startup" do
   expect(docking_station.dock).to eq []
   end
-end
 
-describe DockingStation do
-   it 'returns bike to dock' do
-    bike = Bike.new
-    docking_station = DockingStation.new
-    action = docking_station.dock_bike(bike)
-    expect(action).to eq "docked"
+  it 'checks if bike is returned' do
+    return_bike
+    expect(docking_station.dock.empty?).to be false 
   end
-  
-end
 
-
-describe DockingStation.new.release_bike do
-  it { is_expected.to respond_to(:working?) }
-end
-
-# GOAL - write a test to check whether error is raised when there is no bikes
-describe DockingStation do
-  #ARRANGE
-  # Create an instance of DockingStation.new 
-  docking_station = DockingStation.new
-  #ACT
-  act = docking_station.release_bike
-
-  #ASSERT
-  expect(act).to raise_error("no bike available")
+  it 'releases working bikes' do
+    expect(bike).to be_working
+  end
 end
